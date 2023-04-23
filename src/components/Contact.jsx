cd83e7c062b8e2b4fd9cb139188c543e697fb68b
Abhay sagar kargeti <94851856+FuzzT@users.noreply.github.com>
2023-04-23 18:16:54 +0530

    contact.jsx updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-fu;; h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-      <form action='' method='POST' className='flex flex-col max-w-[600px] w-full'>
+      <form action='https://getform.io/f/688b04d4-6fa4-44c1-b2c7-24755136a6ca' method='POST' className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-[#38b6ff] text-gray-300'>Contact</p>
             <p className='text-gray-300 py-4'>Submit the form velow or send me an email - xyz@gmail.com</p>
